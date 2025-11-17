31a4500223aa64ff3633218288f45eaf75a56ed6
MLGRussianXP <daniilkoles47@gmail.com>
2025-11-17 10:59:00 +0300

    hw #6

---
+++ snakes/snakes/snakesApp.swift
@@ -0,0 +1,17 @@
+//
+//  snakesApp.swift
+//  snakes
+//
+//  Created by dkqz on 17.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct snakesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
