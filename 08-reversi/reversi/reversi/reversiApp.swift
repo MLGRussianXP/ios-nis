46b3322b8ac75b33452abed7bbc76c781eee6ee9
MLGRussianXP <daniilkoles47@gmail.com>
2026-01-21 19:46:46 +0300

    intialize hw #8

---
+++ 08-reversi/reversi/reversi/reversiApp.swift
@@ -0,0 +1,17 @@
+//
+//  reversiApp.swift
+//  reversi
+//
+//  Created by dkqz on 21.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct reversiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ 08-reversi/reversi/reversiTests/reversiTests.swift
@@ -0,0 +1,17 @@
+//
+//  reversiTests.swift
+//  reversiTests
+//
+//  Created by dkqz on 21.01.2026.
+//
+
+import Testing
+@testable import reversi
+
+struct reversiTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
